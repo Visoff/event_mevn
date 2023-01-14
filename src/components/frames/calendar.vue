d3c7d6088e69363a17472aa0eece918fcec8501a
Ilya <ikalinin667@gmail.com>
2023-01-14 20:38:21 +0600

    Working

--- src/components/frames/calendar.vue
+++ src/components/frames/calendar.vue
@@ -11,7 +11,6 @@ export default {
     "name":"calendar-comp",
     data() {
         var calendar = []
-<<<<<<< HEAD
 
         var cursor = new Date()
         cursor.setMonth(cursor.getMonth()+1)
@@ -27,23 +26,6 @@ export default {
             if (i%7 == 0) {calendar.push([])}
             calendar[calendar.length-1].push(cursor.getDate())
             cursor.setDate(cursor.getDate()+1)
-=======
-        var cursor = new Date(); cursor.setDate(1)
-        var month = cursor.getMonth()
-        while (cursor.getDay() != 1) {
-            cursor.setDate(cursor.getDate()-1)
-        }
-        var x = 0
-        var y = 0
-        while (cursor.getMonth() == month) {
-            if (calendar[x] == undefined) {calendar[x] = []}
-            y = 0
-            while (cursor.getDay() != 1) {
-                calendar[x][y] = cursor.getDate()
-                y++
-            }
-            x++
->>>>>>> 87103525a1cf32ca84fc7f2fa63b4c494b97c91d
         }
         return {
             calendar
