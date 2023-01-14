799bed3b7cf6cd08b73e4938c61aafc912cd3676
Ilya <ikalinin667@gmail.com>
2023-01-14 19:56:30 +0600

    Working

---
+++ src/components/frames/calendar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="calendar">
+        <div v-for="row in calendar" :key="row">
+            <div v-for="el in row">{{ el }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    "name":"calendar",
+    data() {
+        var calendar = []
+        var cursor = new Date().setDate(1)
+        var month = cursor.getMonth()
+        while (cursor.getDay() != 1) {
+            cursor.setDate(cursor.getDate()-1)
+        }
+        var x = 0
+        var y = 0
+        while (cursor.getMonth() == month) {
+            if (calendar[x] == undefined) {calendar[x] = []}
+            y = 0
+            while (cursor.getDay() != 1) {
+                calendar[x][y] = cursor.getDate()
+                y++
+            }
+            x++
+        }
+        return {
+            calendar:[]
+        }
+    }
+}
+</script>
