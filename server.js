f1248ee9105dfc9c7c9a6d88fbc023204c33c5d2
Ilya <ikalinin667@gmail.com>
2023-01-15 14:25:55 +0600

    Working

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ client.connect().then(cur => {cursor = cur; on_mongo_connect()})
 async function on_mongo_connect() {
     console.log("mongodb is connected")
     var event = await new oop.Event().from_db(cursor, 1)
-    if (!event.registration.check_person(1)) {
-        event.registration.register_person(1)
+    if (!event.registration.check_person(cursor, 1)) {
+        event.registration.register_person(cursor, 1)
     }
     console.log(event)
 }
