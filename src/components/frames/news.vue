c2ba867490de0b1e10090a948c169346d7523d4b
Ilya <ikalinin667@gmail.com>
2023-01-19 19:39:35 +0600

    Working

--- src/components/frames/news.vue
+++ src/components/frames/news.vue
@@ -22,5 +22,12 @@ export default {
 </script>
 
 <style>
-
+div.news {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: red;
+}
 </style>
